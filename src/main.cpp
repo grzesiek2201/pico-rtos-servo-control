b5f2ead692beadf0bf1d0bac8489dd11f74415cf
grzesiek2201 <grzesiu4237@gmail.com>
2023-11-13 23:54:44 +0100

    functional

---
+++ src/main.cpp
@@ -0,0 +1,221 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+
+#include <FreeRTOS.h>
+#include <FreeRTOSConfig.h>
+#include <task.h>
+
+#include <PCA9685_servo_driver.h>
+#include <PCA9685_servo.h>
+
+
+std::string msg{};
+
+TaskHandle_t gLEDtask = NULL;
+TaskHandle_t gListentask = NULL;
+TaskHandle_t gPrinttask = NULL;
+TaskHandle_t gMoveServotask = NULL;
+TaskHandle_t gFeedbackServotask = NULL;
+
+// SERVO INITIALIZATION
+void init_servo(PCA9685_servo& servo, uint8_t mode, int16_t minRange, 
+                int16_t maxRange, int16_t position, uint8_t address, 
+                uint64_t TConstDur);
+
+PCA9685_servo_driver myController(i2c0, 0, 1, 0x40);
+std::vector<PCA9685_servo> myServo = {PCA9685_servo(&myController, 0, 100, 540),
+                                      PCA9685_servo(&myController, 1, 100, 540)}; // define a vector of servos, note this could extend to use multiple controller on the i2c bus
+uint64_t TNow = 0;
+uint64_t TPrevious = 0;
+uint64_t TEllapsed = 0;
+
+
+void GreenLEDTask(void *param)
+{
+    while(1)
+    {
+        // Blink LED
+        // printf("Blinking!\r\n");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(300));
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(300));
+    }
+}
+
+void listenTask(void *param)
+{
+    while(1)
+    {
+        vTaskDelay(pdMS_TO_TICKS(20));
+        std::getline(std::cin, msg);
+        xTaskNotifyGive(gPrinttask);
+        
+        size_t last = 0; size_t next = 0;
+        next = msg.find(" ", last);
+        std::string mode = msg.substr(last, next-last);
+        last = next + 1;
+        next = msg.find(" ", last);
+
+        if(mode == "e") // for 'encoder'
+        {
+            int nservo = std::stoi(msg.substr(last, next-last));
+
+            std::string enc{};
+            enc += std::to_string(nservo) + " ";
+            enc += (std::to_string(myServo.at(nservo%2).getPosition()*3.14/180));
+            std::cout << enc << " \r\n";
+        }
+        else if(mode == "p") // for 'position'
+        {
+            try
+            {
+                int nservo = std::stoi(msg.substr(last, next-last));
+                last = next + 1;
+                next = msg.find(" ", last);
+                double angle_rad = std::stod(msg.substr(last, next-last));
+                int angle_deg = angle_rad * 180 / 3.14;
+
+                myServo.at(nservo).setPosition(angle_deg);
+            }
+            catch(std::invalid_argument const& ex)
+            {
+                std::cout << "std::invalid_argument::what(): " << ex.what() << "\n";
+            }
+            catch(std::out_of_range const& ex)
+            {
+                std::cout << "std::out_of_range::what(): " << ex.what() << "\n";
+            }
+        }
+    }
+}
+
+void printTask(void *param)
+{
+    int notify_value{0};
+    while(1)
+    {
+        notify_value = ulTaskNotifyTake(pdTRUE, (TickType_t) portMAX_DELAY);
+        if(notify_value > 0)
+        {
+            std::cout << msg << "\n";
+        }
+    }
+}
+
+void servoLoopTask(void *param)
+{
+    while(1)
+    {
+        TNow = time_us_64();			// time now in microseconds
+        TEllapsed = TNow - TPrevious;	// time, in microseconds, since the last loop
+        TPrevious = TNow;				// store this ready for the next loop
+
+        for(auto& servo : myServo)
+        {
+            servo.loop(TEllapsed);
+        }
+        vTaskDelay(pdMS_TO_TICKS(1));
+    }
+}
+
+void servoFeedbackTask(void *param)
+{
+    uint8_t i{0};
+    while(1)
+    {
+        i = 0;
+        for(auto& servo : myServo)
+        {
+            // std::cout << "Servo " << i++ << " position: " << (uint8_t)(servo.getPosition()) << "\n";
+            printf("%d %d \r\n", i++, servo.getPosition());
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+int main()
+{
+    // Initialize chosen serial port
+    stdio_init_all();
+
+    if (cyw43_arch_init()) {
+        printf("WiFi init failed");
+        return -1;
+    }
+    
+    myController.begin(100000);
+    int i{0};
+    for(auto& servo : myServo){
+        init_servo(servo, MODE_SCONSTANT, -90, 90, servo.getMinAngle(), i++, 1000000);
+        servo.setAngularVelocity(145);
+    }
+    sleep_ms(1000);
+
+
+    uint32_t status = xTaskCreate(
+        GreenLEDTask,
+        "Green LED",
+        1024,
+        NULL,
+        tskIDLE_PRIORITY+1,
+        &gLEDtask
+    );
+
+    xTaskCreate(
+        listenTask,
+        "listen",
+        1024,
+        NULL,
+        tskIDLE_PRIORITY,
+        &gListentask
+    );
+
+    // xTaskCreate(
+    //     printTask,
+    //     "print input",
+    //     1024,
+    //     NULL,
+    //     tskIDLE_PRIORITY,
+    //     &gPrinttask
+    // );
+
+    xTaskCreate(
+        servoLoopTask,
+        "servo loop",
+        1024,
+        NULL,
+        tskIDLE_PRIORITY+2,
+        &gMoveServotask
+    );
+
+    // xTaskCreate(
+    //     servoFeedbackTask,
+    //     "servo feedback",
+    //     1024,
+    //     nullptr,
+    //     tskIDLE_PRIORITY,
+    //     &gFeedbackServotask
+    // );
+
+    vTaskStartScheduler();
+
+    // Loop forever
+    while (true) {
+    }
+}
+
+
+void init_servo(PCA9685_servo& servo, uint8_t mode, int16_t minRange, int16_t maxRange, int16_t position, uint8_t address, uint64_t TConstDur)
+{
+    servo.setRange(minRange, maxRange);
+    servo.setMode(mode);
+    servo.setPosition(position); // move to mid point
+    servo.setAddress(address);
+    servo.setTConstantDuration(TConstDur);
+}
